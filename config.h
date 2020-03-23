9c5625795928b3762e6d70ede1f3523da3e3fb76
Jeremy Bobbin <jer@jer.cx>
2020-03-22 19:36:04 -0700

    merge "fifo" bug fix

--- config.h
+++ config.h
@@ -39,7 +39,7 @@ static const Rule rules[] = {
 /* layout(s) */
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
+static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
@@ -69,7 +69,7 @@ static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      {spawn,          {"dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL}}},
 	{ MODKEY|ShiftMask,             XK_Return, {spawn,          {"st", NULL}} },
-	{ MODKEY|ShiftMask,             XK_q,      quit,            {NULL}     },
+	{ MODKEY|ShiftMask,             XK_q,      {quit,           {NULL}}    },
 };
 
 /* button definitions */
