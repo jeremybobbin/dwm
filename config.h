37c42f660d9efb9b44e892fffef9e40d3a4c3e7a
Jeremy Bobbin <jer@jer.cx>
2020-03-22 19:27:03 -0700

    update config.h

--- config.h
+++ config.h
@@ -1,32 +1,26 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static unsigned int borderpx               = 1;        /* border pixel of windows */
-static unsigned int snap                   = 32;       /* snap pixel */
-static int showbar                         = 1;        /* 0 means no bar */
-static int topbar                          = 1;        /* 0 means bottom bar */
-static const char *fonts[]                 = { "monospace:size=10" };
-static const char dmenufont[]              = "monospace:size=10";
-static const char col_gray1[]              = "#222222";
-static const char col_gray2[]              = "#444444";
-static const char col_gray3[]              = "#bbbbbb";
-static const char col_gray4[]              = "#eeeeee";
-static const char col_cyan[]               = "#005577";
-static const unsigned int normfgalpha      = OPAQUE;
-static const unsigned int selfgalpha       = OPAQUE;
-static const unsigned int normborderalpha  = OPAQUE;
-static const unsigned int selborderalpha   = OPAQUE;
-static const unsigned int normbgalpha      = 0xd0;
-static const unsigned int selbgalpha       = 0xd0;
-static const char *colors[][3]      = {
+static unsigned int borderpx   = 1;        /* border pixel of windows */
+static unsigned int snap       = 32;       /* snap pixel */
+static int showbar             = 1;        /* 0 means no bar */
+static int topbar              = 1;        /* 0 means bottom bar */
+static const char *fonts[]     = { "monospace:size=10" };
+static const char dmenufont[]  = "monospace:size=10";
+static const char col_gray1[]  = "#222222";
+static const char col_gray2[]  = "#444444";
+static const char col_gray3[]  = "#bbbbbb";
+static const char col_gray4[]  = "#eeeeee";
+static const char col_cyan[]   = "#005577";
+static const char *colors[][3] = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
 };
 static unsigned int alphas[][3] = {
 	/*               fg      bg        border     */
-	[SchemeNorm] = { normfgalpha, normbgalpha, normborderalpha },
-	[SchemeSel]  = { selfgalpha,  selbgalpha,  selborderalpha  },
+	[SchemeNorm] = { OPAQUE, 0xd0, OPAQUE },
+	[SchemeSel]  = { OPAQUE, 0xd0, OPAQUE },
 };
 
 /* tagging */
@@ -73,41 +67,8 @@ static const char *termcmd[]  = { "st", NULL };
 /* commands */
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
-	/*
 	{ MODKEY,                       XK_p,      {spawn,          {"dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL}}},
 	{ MODKEY|ShiftMask,             XK_Return, {spawn,          {"st", NULL}} },
-	{ MODKEY,                       XK_b,      {togglebar,      {NULL}}    },
-	{ MODKEY,                       XK_j,      {focusstack,     {"+1"}}    },
-	{ MODKEY,                       XK_k,      {focusstack,     {"-1"}}    },
-	{ MODKEY,                       XK_i,      {incnmaster,     {"+1"}}    },
-	{ MODKEY,                       XK_d,      {incnmaster,     {"-1"}}    },
-	{ MODKEY,                       XK_h,      {setmfact,       {"-0.05"}} },
-	{ MODKEY,                       XK_l,      {setmfact,       {"+0.05"}} },
-	{ MODKEY,                       XK_Return, {zoom,           {NULL}}    },
-	{ MODKEY,                       XK_Tab,    {view,           {NULL}}    },
-	{ MODKEY|ShiftMask,             XK_c,      {killclient,     {NULL}}    },
-	{ MODKEY,                       XK_t,      {setlayout,      {"[]="}}   },
-	{ MODKEY,                       XK_f,      {setlayout,      {"><>"}}   },
-	{ MODKEY,                       XK_m,      {setlayout,      {"[M]"}}   },
-	{ MODKEY,                       XK_space,  {setlayout,      {NULL}}    },
-	{ MODKEY|ShiftMask,             XK_space,  {togglefloating, {NULL}}    },
-	{ MODKEY,                       XK_0,      {viewall,        {NULL}}    },
-	{ MODKEY|ShiftMask,             XK_0,      {tagall,         {NULL}}    },
-	{ MODKEY,                       XK_comma,  {focusmon,       {"-1"}}    },
-	{ MODKEY,                       XK_period, {focusmon,       {"+1"}}    },
-	{ MODKEY|ShiftMask,             XK_comma,  {tagmon,         {"-1"}}    },
-	{ MODKEY|ShiftMask,             XK_period, {tagmon,         {"+1"}}    },
-	{ MODKEY,                       XK_F5,     {xresources,     {NULL}}    },
-	TAGKEYS(                        XK_1,                       "0")
-	TAGKEYS(                        XK_2,                       "1")
-	TAGKEYS(                        XK_3,                       "2")
-	TAGKEYS(                        XK_4,                       "3")
-	TAGKEYS(                        XK_5,                       "4")
-	TAGKEYS(                        XK_6,                       "5")
-	TAGKEYS(                        XK_7,                       "6")
-	TAGKEYS(                        XK_8,                       "7")
-	TAGKEYS(                        XK_9,                       "8")
-	*/
 	{ MODKEY|ShiftMask,             XK_q,      quit,            {NULL}     },
 };
 
@@ -153,7 +114,6 @@ static Cmd commands[] = {
 	/* create [cmd]: create a new window, run `cmd` in the shell if specified */
 	/*{ "create", { create,	{ NULL } } },*/
 	/* focus <win_id>: focus the window whose `DVTM_WINDOW_ID` is `win_id` */
-	{ "create",         { spawn,          { NULL } } },
 	{ "focusmon",       { focusmon,       { NULL } } },
 	{ "focusstack",     { focusstack,     { NULL } } },
 	{ "killclient",     { killclient,     { NULL } } },
